5c306b90dbf6d298ab872964715be064654ffd76
Priyanshu Maurya <106907193+satyasaadhika@users.noreply.github.com>
2024-08-24 23:16:21 +0530

    Update settings.py
    Add '.vercel.app' to allowed hosts

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-45nkc@v^o$dp-fmf*$x020f6)(%djfcau@s51itkm(7o9l)x0f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
